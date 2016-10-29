49d4e6bacf7c422d8bcd0992f1a71944ececd1df
bartoszl <barteklewandowski94@gmail.com>
2016-10-29 20:00:00 +0100

    integrated sqlalchemy and flask script, added migration script

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,20 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_script import Manager, Shell
+from flask_migrate import Migrate, MigrateCommand
 
 app = Flask(__name__)
-from app import views
+app.config.from_object('config')
+
+manager = Manager(app)
+
+db = SQLAlchemy(app)
+
+from app import views, models
+
+migrate = Migrate(app, db)
+
+def make_shell_context():
+    return dict(app=app, db=db, Station=models.Station, Measurment=models.Measurment)
+manager.add_command("shell", Shell(make_context=make_shell_context))
+manager.add_command('db', MigrateCommand)
