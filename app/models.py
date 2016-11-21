368b511877e1eb560a5a334d73f40494549dfe15
Bartosz Lewandowski <bartosz@bartoszs-mbp.home>
2016-11-21 01:03:34 +0000

    some initial changes to river station/level models

--- app/models.py
+++ app/models.py
@@ -1,24 +1,23 @@
 from app import db
+from sqlalchemy.sql import func
 
-class Station(db.Model):
-  __tablename__ = 'stations'
-  id = db.Column(db.Integer, primary_key=True)
-  office = db.Column(db.String(64))
-  station_name = db.Column(db.String(64), unique=True, index=True)
+class RiverStation(db.Model):
+  __tablename__ = 'RiverStation'
+  riverstation_id = db.Column(db.Integer, primary_key=True, nullable=False)
+  riverstation_uid = db.Column(db.String(64), unique=True, index=True, nullable=False)
   location_code = db.Column(db.Integer, unique=True, index=True)
   measurments = db.relationship('Measurment', backref='station')
 
   def __repr__(self):
-    return '<Station id:%d, name:%r' % (self.id, self.station_name)
+    return '<Station id:%d, name:%r' % (self.riverstation_id, self.riverstation_uid)
 
-class Measurment(db.Model):
-  __tablename__= 'measurments'
-  id = db.Column(db.Integer, primary_key=True)
-  station_id = db.Column(db.Integer, db.ForeignKey('stations.id'))
-  date_time = db.Column(db.DateTime)
-  value = db.Column(db.Float)  
+class RiverLevel(db.Model):
+  __tablename__= 'RiverLevel'
+  riverlevel_id = db.Column(db.Integer, primary_key=True, nullable=False)
+  riverstation_id = db.Column(db.Integer, db.ForeignKey('stations.id'), nullable=False)
+  sampled_at = db.Column(db.DateTime, nullable=False)
+  archived_at = db.Column(db.DateTime, default=func.now(), nullable=False)
+  level = db.Column(db.Float, nullable=False)
 
   def __repr__(self):
     return '<Measurment id:%d, date:%c, value:%0.3f' % (self.id, self.date_time, self.value)
-
-
